8bbffaa450aa6f61d7637d2ced457cc8ba91fff1
caefleury <caefleury15@gmail.com>
2023-10-13 17:50:33 -0300

    setters and getters

---
+++ src/main/java/br/com/caefleury/todolist/user/UserController.java
@@ -0,0 +1,23 @@
+package br.com.caefleury.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.print(userModel.getUsername());
+    }
+}
